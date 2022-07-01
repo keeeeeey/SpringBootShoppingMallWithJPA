7a786f2cb79bd10e1a7026686a477bdaf598bda8
keeeeeey <sseioul@naver.com>
2022-07-01 20:42:16 +0900

    [PRACTICE] thymeleaf practice

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -49,4 +49,41 @@ public class ThymeleafExController {
         model.addAttribute(itemDtoList);
         return "thymeleafEx/thymeleafEx03";
     }
+
+    @GetMapping(value = "ex04")
+    public String thymeleafExample04(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setItemNm("테스트 상품" + i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute(itemDtoList);
+        return "thymeleafEx/thymeleafEx04";
+    }
+
+    @GetMapping(value = "ex05")
+    public String thymeleafExample05(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setItemNm("테스트 상품" + i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute(itemDtoList);
+        return "thymeleafEx/thymeleafEx05";
+    }
+
 }
